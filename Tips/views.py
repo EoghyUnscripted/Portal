4cb2362fc455c9091c92ae286bbd64e50fe4a439
Eoghan <eoghan@hulbert.info>
2021-02-01 00:19:50 -0800

    Modifications to the layout and form functions.

--- Tips/views.py
+++ Tips/views.py
@@ -15,16 +15,27 @@ def dashboard(request):
 @login_required(login_url='/login')
 def tips(request):
     if request.method == 'POST':
-        if (request.POST['check_amt'] and request.POST['service_lvl'] == 'Custom'):
-            check = float(request.POST['check_amt'])
-            tip = check * (float(request.POST['custom_percent']) / 100)
-            calc = check + tip
-            pay = float(calc)
-            percent = str(request.POST['custom_percent']) + '%'
+        if request.POST['service_lvl'] == '---':
             service = Percentage.objects
-            context = {'pay':pay, 'tip':tip, 'check':check, 'percent':percent, 'service':service}
+            error = 'You must select a service level or custom percentage for manual entry.'
+            context = {'service':service, 'error':error}
             return render(request, 'tips/dashboard.html', context)
-        elif (request.POST['check_amt'] and request.POST['service_lvl']):
+        elif request.POST['service_lvl'] == 'Custom':
+            if (request.POST['check_amt'] and request.POST['custom_percent']):
+                check = float(request.POST['check_amt'])
+                tip = check * (float(request.POST['custom_percent']) / 100)
+                calc = check + tip
+                pay = float(calc)
+                percent = str(request.POST['custom_percent']) + '%'
+                service = Percentage.objects
+                context = {'pay':pay, 'tip':tip, 'check':check, 'percent':percent, 'service':service}
+                return render(request, 'tips/dashboard.html', context)
+            else:
+                service = Percentage.objects
+                error = 'Check amount and service level are required.\\nFor custom service level, include the custom percent.'
+                context = {'service':service, 'error':error}
+                return render(request, 'tips/dashboard.html', context)
+        elif (request.POST['check_amt'] and request.POST['service_lvl'] != '-1'):
             service = get_object_or_404(Percentage, pk=request.POST['service_lvl'])
             percent = service.Tip_Percent
             check = float(request.POST['check_amt'])
